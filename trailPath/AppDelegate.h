b7d2a87dc5a7403b6730c5e6776b92c857ce2891
Hamish Knight <hk@southerncare.co.uk>
2016-01-19 15:38:52 +0000

    Initial Commit

---
+++ trailPath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  trailPath
+//
+//  Created by Hamish Knight on 19/01/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ trailPath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  trailPath
+//
+//  Created by Hamish Knight on 19/01/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
